3efabee5daee696362ae549ace4697b853513250
Tanto <99785440+Egruda@users.noreply.github.com>
2023-05-26 08:09:07 +0700

    Create +-*/ functions and basic calculator html

---
+++ script.js
@@ -0,0 +1,19 @@
+let x;
+let y;
+let operator;
+
+function add(x,y) {
+    return x+y;
+}
+
+function substract(x,y) {
+    return x-y;
+}
+
+function multiply(x,y) {
+    return x*y;
+}
+
+function divide(x,y) {
+    return x/y;
+}
